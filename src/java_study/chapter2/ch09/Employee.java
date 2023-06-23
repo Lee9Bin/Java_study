096f37b7255a38f152374a7b0954b93a183a42e5
Lee9Bin <imagen33@daum.net>
2023-06-23 21:24:39 +0900

    인스턴스 변수

---
+++ src/java_study/chapter2/ch09/Employee.java
@@ -0,0 +1,41 @@
+package java_study.chapter2.ch09;
+
+public class Employee {
+//  static변수는 공통으로 사용하는 변수가 필요할때 사용 ex)사번, 학번 등
+//  인스턴스가 생성될 때 만들어지는 변수가 아닌, 처음 프로그램이 메모리 로딩 될때 할당
+
+    public static int serialNum = 100;
+    private int employeeId;
+    private String employeeName;
+    private String deparment;
+
+    public Employee(){
+        employeeId = serialNum;
+        serialNum ++;
+    }
+
+//  private로 선언 했으니 게터셋터 gogogog!
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getDeparment() {
+        return deparment;
+    }
+
+    public void setDeparment(String deparment) {
+        this.deparment = deparment;
+    }
+}
---
+++ src/java_study/chapter2/ch09/EmployeeTest.java
@@ -0,0 +1,26 @@
+package java_study.chapter2.ch09;
+
+public class EmployeeTest {
+    public static void main(String[] args){
+//      홍길동에 serialnum을 empliyeeId에 넣고 serialnum은 1증가
+        Employee employeeLee = new Employee();
+        employeeLee.setEmployeeName("홍길동");
+//      그러고 나서 serialnum을 출력하고 나면 101
+        System.out.println(employeeLee.serialNum);
+
+//      김흥부에 serialnum을 empliyeeId에 넣고 serialnum은 1증가
+        Employee employeeKim = new Employee();
+        employeeKim.setEmployeeName("김흥부");
+//      그러고 나서 serialnum은 102
+//      이와 같이 두개의 인스턴스가 하나의 메모리를 공유
+//      데이터 영역      스택 메모리     힙 메모리(동적메모리)
+//      static변수(serialNum) <-- employeeLee --> employeeLee인스턴스
+//          ^------------------ employeeKim --> employeeKim인스턴스
+        System.out.println(employeeLee.serialNum);//      102
+        System.out.println(employeeKim.serialNum);//      102
+//      생성자를 통해 static 변수를 증가시켜 사번을 부여
+        System.out.println(employeeLee.getEmployeeId());//   100
+        System.out.println(employeeKim.getEmployeeId());//   101
+
+    }
+}
