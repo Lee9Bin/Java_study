0ca86421724be7693c135aabfe2cc95587d1a933
Lee9Bin <imagen33@daum.net>
2023-07-03 19:20:03 +0900

    인터페이스 활용 실습하기

---
+++ src/java_study/chapter3/ch12/RoundRobin.java
@@ -0,0 +1,14 @@
+package java_study.chapter3.ch12;
+
+public class RoundRobin implements Scheduler{
+
+    @Override
+    public void getNextCall() {
+        System.out.println("상담 전화를 순서대로 대기열에서 가져옵니다");
+    }
+
+    @Override
+    public void sendCallToAgent() {
+        System.out.println("다음 순숴의 상담원에게 배분합니다.");
+    }
+}
---
+++ src/java_study/chapter3/ch12/Scheduler.java
@@ -0,0 +1,6 @@
+package java_study.chapter3.ch12;
+
+public interface Scheduler {
+    void getNextCall();
+    void sendCallToAgent();
+}
