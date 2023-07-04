2019ff2ec17822d5f1ee5715e3cf69373b2318b4
Lee9Bin <imagen33@daum.net>
2023-07-04 20:54:21 +0900

    ArrayList 활용

---
+++ src/java_study/chapter5/ch01/MemberArrayListTest.java
@@ -0,0 +1,24 @@
+package java_study.chapter5.ch01;
+
+public class MemberArrayListTest {
+    public static void main(String[] args) {
+        MemberArrayList memberArrayList = new MemberArrayList();
+
+        Member memberL = new Member(1001,"lee");
+        Member memberK = new Member(1001,"kim");
+        Member memberP = new Member(1001,"park");
+        Member memberS = new Member(1001,"son");
+        Member memberG = new Member(1001,"gang");
+
+        memberArrayList.addMember(memberL);
+        memberArrayList.addMember(memberK);
+        memberArrayList.addMember(memberP);
+        memberArrayList.addMember(memberS);
+        memberArrayList.addMember(memberG);
+
+        memberArrayList.showAllMember();
+
+        memberArrayList.removeMember(memberP.getMemberId());
+        memberArrayList.showAllMember();
+    }
+}
