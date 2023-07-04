0b59b6b55a6f41e8dc1d4dab3c31e3b44bbca061
Lee9Bin <imagen33@daum.net>
2023-07-04 21:52:42 +0900

    TreeSet 클래스 활용

---
+++ src/java_study/chapter5/ch03/Member.java
@@ -0,0 +1,66 @@
+package java_study.chapter5.ch03;
+
+public class Member implements Comparable<Member>{
+    private int memberId;        //회원 아이디
+    private String memberName;   //회원 이름
+
+    public Member(int memberId, String memberName){ //생성자
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public int getMemberId() {  //
+        return memberId;
+    }
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+    public String getMemberName() {
+        return memberName;
+    }
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    @Override
+    public String toString(){   //toString 메소드 오버로딩
+        return memberName + " 회원님의 아이디는 " + memberId + "입니다";
+    }
+    @Override
+    public int hashCode() {
+        return memberId;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if( obj instanceof Member){
+            Member member = (Member)obj;
+            if( this.memberId == member.memberId )
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+//  Comparable 인터페이스 구현
+    @Override
+    public int compareTo(Member member) {
+//      아이디 기준으로 오름차순으로 정렬
+        if (this.memberId > member.memberId){
+            return 1;
+        }
+        else if (this.memberId < member.memberId){
+            return -1;
+        }
+        else {
+        return 0;
+        }
+    }
+//  Comparator인터페이스 구현 하는 경우
+//  이 인터페이스를 할 경우 treeSet = new TreeSet<>();에서 반드시<> ()에 내가 어떤것으로 구현했는지 명시해야해
+//  그래서 보통 Comparable을 일반적으로 사용을 해 그럼 언제 쓰냐? 이미 Comparable이 구현 된 경우 Comparator로 비교하는 방식을 다시 구현 가능
+//    @Override
+//    public int compare(Member member1, Member member2){
+//        return (member1.memberId - member2.memBerId2);
+//    }
+}
---
+++ src/java_study/chapter5/ch03/MemberTreeSetTest.java
@@ -0,0 +1,28 @@
+package java_study.chapter5.ch03;
+
+import java.util.TreeSet;
+
+public class MemberTreeSetTest {
+//  정렬이 가능한 set클래스
+    public static void main(String[] args) {
+//        TreeSet<String> set = new TreeSet();
+//
+//        set.add("b");
+//        set.add("a");
+//        set.add("e");
+//        set.add("z");
+//        System.out.println(set);
+
+        MemberTreeSet memberTreeSet = new MemberTreeSet();
+
+        Member memberKim = new Member(1003, "z");
+        Member memberLee = new Member(1001, "a");
+        Member memberKang = new Member(1002, "c");
+
+        memberTreeSet.addMember(memberKim);
+        memberTreeSet.addMember(memberLee);
+        memberTreeSet.addMember(memberKang);
+        memberTreeSet.showAllMember();
+
+    }
+}
