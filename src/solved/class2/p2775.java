03c4f3930752653ad591b3c7a7aabb9dbe8f95e7
Lee9Bin <imagen33@daum.net>
2023-07-12 17:45:21 +0900

    [백준] 2775번

---
+++ src/solved/class2/p2775.java
@@ -0,0 +1,32 @@
+package solved.class2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p2775 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int[][] apart = new int[a+1][b+1];
+
+            for (int cmd = 0; cmd<a+1; cmd++){
+                for (int ho = 1; ho<b+1; ho++){
+                    if(ho == 1){
+                        apart[cmd][ho] = 1;
+                    }
+                    if (cmd == 0){
+                        apart[cmd][ho] = ho;
+                    }
+                    else if (cmd != 0) {
+                        apart[cmd][ho] = apart[cmd][ho-1] + apart[cmd-1][ho];
+                    }
+                }
+            }
+            System.out.println(apart[a][b]);
+        }
+    }
+}
