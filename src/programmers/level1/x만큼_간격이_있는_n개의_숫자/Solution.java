fb52680a2931901ef5653b2b57c7e6e9231ba821
Lee9Bin <imagen33@daum.net>
2023-07-13 16:54:39 +0900

    [프로그래머스] x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/level1/x만큼_간격이_있는_n개의_숫자/Solution.java
@@ -0,0 +1,13 @@
+package programmers.level1.x만큼_간격이_있는_n개의_숫자;
+
+import java.util.Arrays;
+
+public class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++){
+            answer[i] = (long)x * (long)(i+1);
+        }
+        return answer;
+    }
+}
