1f8d43a47b820a279e7cf604055a19a4b3a36e57
Lee9Bin <imagen33@daum.net>
2023-07-20 19:19:09 +0900

    [프로그래머스] 가운데 글자 가져오기

---
+++ src/programmers/level1/가운데_글자_가져오기/Solution.java
@@ -0,0 +1,14 @@
+package programmers.level1.가운데_글자_가져오기;
+
+public class Solution {
+    public String solution(String s) {
+        String answer;
+        if (s.length() % 2 == 0){
+            answer = s.substring(s.length()/2-1,s.length()/2+1);
+        }
+        else {
+            answer = s.substring(s.length()/2,s.length()/2+1);
+        }
+        return answer;
+    }
+}
