8ab06f692cf2fc9cc9389d157be53d27976cc3e1
Lee9Bin <imagen33@daum.net>
2024-03-03 12:46:32 +0900

    [백준] 9372번

---
+++ src/solved/silver/P9372.java
@@ -0,0 +1,27 @@
+package solved.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P9372 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < testCase; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j<m; j++){
+                st = new StringTokenizer(br.readLine());
+            }
+
+            sb.append(n-1).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
