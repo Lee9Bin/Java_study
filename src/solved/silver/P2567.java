b2dc8c1fe1e5b2b48a81496ddbdb700e3144da94
Lee9Bin <imagen33@daum.net>
2024-04-21 19:05:50 +0900

    [백준] 2567번

---
+++ src/solved/silver/P2567.java
@@ -0,0 +1,54 @@
+package solved.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P2567 {
+
+    public static final int[] dx = {-1,0,1,0};
+    public static final int[] dy = {0,1,0,-1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] map = new int[101][101];
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < n ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++){
+                for (int k = y; k < y + 10; k++){
+                    map[j][k] = 1;
+                }
+            }
+        }
+
+        // for (int[] ints : map) {
+        //     System.out.println(Arrays.toString(ints));
+        // }
+        int total = 0;
+        for (int i = 0; i < 101; i++){
+            for (int j = 0; j < 101; j++){
+                if (map[i][j] == 1){
+                    for (int k = 0; k < 4; k++){
+                        int nextX = i + dx[k];
+                        int nextY = j + dy[k];
+                        if (nextX < 0 || nextX >= 101 || nextY < 0 || nextY >= 101) {
+                            total ++;
+                            continue;
+                        }
+                        if (map[nextX][nextY] == 0) total ++;
+                    }
+                }
+            }
+        }
+        System.out.println(total);
+    }
+}
