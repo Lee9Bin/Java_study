8630912d877c2a5519613fc19626f8491e8394e1
Lee9Bin <imagen33@daum.net>
2024-08-17 15:19:11 +0900

    [백준] 18353번

---
+++ src/solved/silver/P18353.java
@@ -0,0 +1,36 @@
+package solved.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P18353 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int result = 0;
+        for (int i : dp) {
+            result = Math.max(result, i);
+        }
+        System.out.println(n - result);
+    }
+}
