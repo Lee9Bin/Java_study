35692b027744837d26767c56c6413555d4bf3d45
Lee9Bin <imagen33@daum.net>
2025-03-03 19:01:30 +0900

    [백준] 13398번

---
+++ src/solved/gold/P13398.java
@@ -0,0 +1,34 @@
+package solved.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P13398 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N + 1];
+
+		for (int i = 1; i < N + 1; i++)
+			nums[i] = Integer.parseInt(st.nextToken());
+
+		int[][] dp = new int[N + 1][2];
+		dp[1][0] = nums[1];
+		dp[1][1] = nums[1];
+
+		int result = Integer.MIN_VALUE;
+		for (int i = 2; i < N + 1; i++) {
+			dp[i][0] = Math.max(dp[i - 1][0] + nums[i], nums[i]);
+			dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + nums[i]);
+			result = Math.max(dp[i][0], result);
+			result = Math.max(dp[i][1], result);
+		}
+
+		System.out.println(result);
+
+	}
+}
