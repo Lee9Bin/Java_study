9dfc218c6ddddba7495d5a931f04a1db2ab36b51
Lee9Bin <imagen33@daum.net>
2025-03-03 20:52:57 +0900

    [백준] 13398번

--- src/solved/gold/P13398.java
+++ src/solved/gold/P13398.java
@@ -20,7 +20,7 @@ public class P13398 {
 		dp[1][0] = nums[1];
 		dp[1][1] = nums[1];
 
-		int result = Integer.MIN_VALUE;
+		int result = nums[1];
 		for (int i = 2; i < N + 1; i++) {
 			dp[i][0] = Math.max(dp[i - 1][0] + nums[i], nums[i]);
 			dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + nums[i]);
